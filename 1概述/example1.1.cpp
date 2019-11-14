42bb8dec92de101d600ca80fe3e4bcc32a5d3b21
begger123 <48647767+begger123@users.noreply.github.com>
2019-11-14 16:01:25 +0800

    Update example1.1.cpp

--- 1概述/example1.1.cpp
+++ 1概述/example1.1.cpp
@@ -1,1 +1,16 @@
 
+ 目前最流行的Ｃ语言有以下几种：    
+  ·Microsoft C 或称 MS C     
+  ·Borland Turbo C 或称 Turbo C    
+  ·AT&TC
+  注意：关键字都是小写的
+  
+ #include<math.h>
+ #include<stdio.h>
+main（）
+{ double x,s;
+printf("input nember:\n")
+scanf("%lf",&x);
+s=sin(x)
+printf("sine of %lf is %lf\n",x,s)
+}
